686d67405c6084b192dca124c1650a72322bd697
Jeffrey <obstructor@hotmail.com>
2019-02-12 14:21:44 -0700

    node server now serves up an html file

--- Code/server.js
+++ Code/server.js
@@ -1,14 +1,25 @@
 /*server.js*/
 
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+
+  fs.readFile("html/index.html",function(error,pgResp)
+    {
+      if(error)
+      {
+        res.writeHead(404);
+        res.write("index not found");
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(pgResp);
+      }
+      res.end();
+    });
 });
 
 server.listen(port, hostname, function() {

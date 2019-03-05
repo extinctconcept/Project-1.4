dbf6a537bda38cf6b72ad03f3bf0566c5513726c
Jeffrey Michaelis <jeffrey.michaelis@aggiemail.usu.edu>
2019-03-05 11:06:45 -0700

    formatting

--- Code/login.js
+++ Code/login.js
@@ -3,16 +3,16 @@ const URL = require('url');
 const FS = require('fs');
 
 
-function Query_Login(username,password)
+function Query_Login(username, password)
 {
     return username == 'test' && password == 'test';
 }
 
 module.exports.login = function(req, res, next)
 {
-    var query_data = URL.parse(req.url,true).query;
+    var query_data = URL.parse(req.url, true).query;
     console.log(query_data);
-    if(Query_Login(query_data.username,query_data.password))
+    if(Query_Login(query_data.username, query_data.password))
     {
         EXPRESS.static("html/static/profile.html")(req,res,next);
         // really we want to shove information into this instead of doing a static serve

a23f04b24fcfa9ccbf1827232c9f1325f0bae15b
Brandon Larsen <larsen.brand@outlook.com>
2019-04-01 19:32:04 -0600

    adding new update to sql create, it now works as it should

--- Code/sql/create/create.sql
+++ Code/sql/create/create.sql
@@ -1,18 +1,19 @@
-CREATE TABLE persons
+CREATE TABLE public.persons
 (
   person_id     integer    NOT NULL,
-  pw            varchar,
+  pw            varchar	   NOT NULL,
   first_name    varchar    NOT NULL,
   last_name     varchar    NOT NULL,
   email         varchar    NOT NULL,
   phone         integer    NOT NULL,
   city          varchar    NOT NULL,
   state         varchar    NOT NULL,
-  zip           integer    NOT NULL
-  PRIMARY KEY (person_id),
+  zip           integer    NOT NULL,
+  CONSTRAINT person_id PRIMARY KEY (person_id),
+  CONSTRAINT person_id UNIQUE (person_id)
 );
 
-CREATE TABLE game
+CREATE TABLE public.game
 (
   game_id       integer     NOT NULL,
   person_id     integer     NOT NULL,
@@ -21,11 +22,12 @@ CREATE TABLE game
   art_url       varchar,
   rating        integer,
   availability_id integer,
-  PRIMARY KEY (game_id),
+  CONSTRAINT game_id PRIMARY KEY (game_id),
   CONSTRAINT person_id_fkey FOREIGN KEY (person_id)
+	 REFERENCES public.persons (person_id) MATCH FULL
 );
 
-CREATE TABLE exchange
+CREATE TABLE public.exchange
 (
   exchange_id   integer     NOT NULL,
   owner_id      integer     NOT NULL,
@@ -33,15 +35,19 @@ CREATE TABLE exchange
   game_id       integer     NOT NULL,
   exchange_date timestamp without time zone,
   return_date   timestamp without time zone,
-  PRIMARY KEY (exchange_id),
-  CONSTRAINT owner_id_fkey    FOREIGN KEY (person_id)
-  CONSTRAINT borrower_id_fkey FOREIGN KEY (person_id)
+  CONSTRAINT exchange_id 	  PRIMARY KEY (exchange_id),
+  CONSTRAINT owner_id_fkey    FOREIGN KEY (owner_id)
+	 REFERENCES public.persons (person_id) MATCH FULL,
+  CONSTRAINT borrower_id_fkey FOREIGN KEY (borrower_id)
+	 REFERENCES public.persons (person_id) MATCH FULL,
   CONSTRAINT game_id_fkey     FOREIGN KEY (game_id)
+	 REFERENCES public.game (game_id) MATCH FULL
 );
 
-CREATE TABLE game_availability
+CREATE TABLE public.game_availability
 (
   available integer NOT NULL,
   unavailable integer NOT NULL,
   pending integer NOT NULL
 );
+

3b612f8c58d25542d96131e543cb5491577bbdbc
Marc Bigler <mbigler@cmpdesigners.com>
2019-04-08 16:19:28 -0600

    Games

--- Code/html/static/loadgames.js
+++ Code/html/static/loadgames.js
@@ -1,2 +1,18 @@
+var data = null;
 
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("GET", "https://api-v3.igdb.com/games/?fields=name%2Ccover.url%2Cgenres.name");
+xhr.setRequestHeader("cookie", "__cfduid=dbfedfdd2af3b8fd49a4dc01195a5a6841554437156");
+xhr.setRequestHeader("user-key", "b75f7df0294c1201e3e195ee8a453ca9");
+xhr.setRequestHeader("accept", "application/json");
+
+xhr.send(data);
 

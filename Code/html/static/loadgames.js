a9cc11ffb2d089e068ef9305a0c116e292396b63
Jeffrey Michaelis <jeffrey.michaelis@aggiemail.usu.edu>
2019-04-08 17:40:07 -0600

    home page queries games now

--- Code/html/static/loadgames.js
+++ Code/html/static/loadgames.js
@@ -1,11 +1,25 @@
 var data = null;
 
 var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+
+
+function Display_Games(arr)
+{
+    let display = document.getElementById("game-display");
+    display.innerHTML = "<p>TOP GAMES!!!!!!:</p>";
+    arr.forEach(element => {
+        if(element.cover === undefined)
+            display.innerHTML += "<p>" + element.name + "<p>";
+        else
+            display.innerHTML += "<p>" + "<img src='" + element.cover.url + "'>" + element.name + " " + "<p>";
+    });
+}
 
 xhr.addEventListener("readystatechange", function () {
   if (this.readyState === this.DONE) {
-    console.log(this.responseText);
+    let x = JSON.parse(this.responseText);
+    console.log(x);
+    Display_Games(x);
   }
 });
 
--- Code/server.js
+++ Code/server.js
@@ -7,6 +7,7 @@ const LOGIN = require("./login.js");
 const COOKIES = require("./cookie.js");
 const PROFILE = require("./profile.js");
 const CORS = require('cors');
+const REQUEST = require("request");
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -20,26 +21,24 @@ var contentTypesByExtension = {
 
 function Query_Games_Api(req,res,next)
 {
-  var request = require("request");
+  var jar = REQUEST.jar();
+  jar.setCookie(REQUEST.cookie("__cfduid=dbfedfdd2af3b8fd49a4dc01195a5a6841554437156"), "https://api-v3.igdb.com/games/");
 
-var jar = request.jar();
-jar.setCookie(request.cookie("__cfduid=dbfedfdd2af3b8fd49a4dc01195a5a6841554437156"), "https://api-v3.igdb.com/games/");
+  var options = { method: 'GET',
+    url: 'https://api-v3.igdb.com/games/',
+    qs: { fields: 'name,cover.url,genres.name'},
+    headers:
+    { 'user-key': 'b75f7df0294c1201e3e195ee8a453ca9',
+      accept: 'application/json' },
+    jar: 'JAR' };
 
-var options = { method: 'GET',
-  url: 'https://api-v3.igdb.com/games/',
-  qs: { fields: 'name,cover.url,genres.name' },
-  headers:
-   { 'user-key': 'b75f7df0294c1201e3e195ee8a453ca9',
-     accept: 'application/json' },
-  jar: 'JAR' };
+  REQUEST(options, function (error, response, body) {
+    if (error) throw new Error(error);
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
+    res.send(body);
+  });
 
-  console.log(body);
-});
 
-  
 }
 
 //app.use(CORS({origin: '*'}));
@@ -51,9 +50,9 @@ app.use('/logout|/logout.html', LOGIN.logout);
 app.use('/profile.html', PROFILE.profile);
 app.use('/profile/:username', PROFILE.view_profile);
 app.use(EXPRESS.static("html/static"));
+app.use('/games', Query_Games_Api);
 app.use(function (req, res, next) {
   res.redirect("/index.html");
 })
-app.use(Query_Games_Api);
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))

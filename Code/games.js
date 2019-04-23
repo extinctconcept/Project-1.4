76c014752f13fb656b571ae64fdd81ce7c6093bc
Jeffrey <obstructor@hotmail.com>
2019-04-23 17:28:35 -0600

    added underscores

--- Code/games.js
+++ Code/games.js
@@ -1,11 +1,11 @@
 const DB = require('./queries');
 
-module.exports.requestgame = function(req,res,next)
+module.exports.request_game = function(req,res,next)
 {
 
 }
 
-module.exports.allgames = function(req,res,next)
+module.exports.all_games = function(req,res,next)
 {
     DB.getGames((result) =>{
         res.status(200);
